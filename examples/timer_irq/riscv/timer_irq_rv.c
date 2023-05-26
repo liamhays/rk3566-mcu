429522ec09a18d1cc64dc42ce7ab74e784dd67b0
Liam Hays <liamrhays@gmail.com>
2023-05-26 18:57:03 +0000

    Working timer interrupt in the MCU! No idea if it can fire more than once yet.

--- examples/timer_irq/riscv/timer_irq_rv.c
+++ examples/timer_irq/riscv/timer_irq_rv.c
@@ -5,16 +5,31 @@
 #define MAILBOX_B2A_DAT_0 (MAILBOX_BASE + 0x0034)
 
 
+// also the "INTC" base in Rockchip register space
+#define MCU_BASE (0xFE790000)
+// timers are memory mapped CSRs
+#define MCU_TIMER_CTRL (MCU_BASE + 0x0000)
+// the first six MCU registers are just memory locations, not CSRs.
+#define MCU_MTIMECMP (MCU_BASE + 0x0010)
+// timers are rewritable
+#define MCU_MTIME (MCU_BASE + 0x0008)
+
+
 volatile uint32_t* mailbox_b2a_cmd_0 = (uint32_t*)MAILBOX_B2A_CMD_0;
+volatile uint64_t* mtimecmp = (uint64_t*)MCU_MTIMECMP;
+volatile uint64_t* mtime = (uint64_t*)MCU_MTIME;
 
 // Under GCC, interrupt functions must have the interrupt attribute
 static void irq_entry() __attribute__ ((interrupt ("machine")));
 
 // alignment for mtvec.BASE
 #pragma GCC push_options
-#pragma GCC optimize ("align-functions=4")
+// align on a 64-byte boundary because mtvec.BASE is only upper 26
+// bits (32-26 = 6, 2**6 = 64)
+#pragma GCC optimize ("align-functions=64")
 void irq_entry() {
   uint32_t cause;
+
   // in direct mode, you have to read mcause
   __asm__ volatile ("csrr     %0,mcause"
 		    : "=r" (cause) // output into cause
@@ -28,21 +43,16 @@ void irq_entry() {
   
   // update mailbox register
   *mailbox_b2a_cmd_0 = 0xabababab;
+
 }
 
-// also the "INTC" base in Rockchip register space
-#define MCU_BASE (0xFE790000)
-// timers are memory mapped CSRs
-#define MCU_TIMER_CTRL (MCU_BASE + 0x0000)
-// the first six MCU registers are just memory locations, not CSRs.
-#define MCU_MTIMECMP (MCU_BASE + 0x0010)
 int main() {
   // default value to make change visible
   *mailbox_b2a_cmd_0 = 0x11111111;
   
   // set vector address in mtvec and use direct mode
   // I think the IPIC can vector interrupts, but I haven't tested that yet.
-  uint32_t mtvec_value = (((uint32_t)&irq_entry) << 6);
+  uint32_t mtvec_value = (((uint32_t)&irq_entry >> 6) << 6);
   __asm__ volatile ("csrw     mtvec, %0"
 		    : // no output
 		    : "r" (mtvec_value) // write mtvec_value to %0
@@ -58,14 +68,26 @@ int main() {
 		    : // no clobber
 		    );
 
+  // enable global interrupts
+  // this is causing the whole system to crash: probably causing a bad
+  // access somewhere
+  uint32_t mie_enable = 0b1000;
+  __asm__ volatile ("csrrs    zero, mstatus, %0"
+		    : // no output
+		    : "r" (mie_enable)
+		    :
+		    );
   // timer is enabled by default according to TRM
   // timer divider defaults to one tick per every clock tick
 
   // set low 32 bits of MTIMECMP to 0x100 (something low)
-  volatile uint64_t* mtimecmp = (uint64_t*)MCU_MTIMECMP;
-  // prevent problems caused by intermediate values
+  
+  // writing all 1s prevent problems caused by intermediate values
   *mtimecmp = 0xffffffffffffffff;
   *mtimecmp = 0x100;
+
+  // reset the timer so the interrupt actually fires
+  *mtime = 0;
   while (1);
 
   return 0;
