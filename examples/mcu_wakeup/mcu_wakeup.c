75d3b7cabbdcb3f1d1d2216bdfa0b70924569aec
Liam Hays <liamrhays@gmail.com>
2023-07-16 15:48:37 -0600

    Add mcu_wakeup, make it work (just blink LED for now), and show that MCU does not stay enabled during suspend by default.

---
+++ examples/mcu_wakeup/mcu_wakeup.c
@@ -0,0 +1,161 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/io.h>
+#include <linux/regmap.h>
+#include <linux/ioport.h>
+
+#include <linux/pinctrl/consumer.h>
+
+#include "riscv/build/mcu_wakeup_rv_bin.h"
+
+#define SYS_GRF_BASE (0xFDC60000)
+
+#define GRF_SOC_CON3 (SYS_GRF_BASE + 0x50C)
+#define GRF_SOC_CON3_LEN (4)
+#define GRF_SOC_CON4 (SYS_GRF_BASE + 0x510)
+#define GRF_SOC_CON4_LEN (4)
+
+#define CRU_BASE (0xFDD20000)
+
+#define CRU_GATE_CON32 (CRU_BASE + 0x380)
+#define CRU_GATE_CON32_LEN (4)
+#define CRU_SOFTRST_CON26 (CRU_BASE + 0x0468)
+#define CRU_SOFTRST_CON26_LEN (4)
+
+#define GPIO0_BASE (0xFDD60000)
+#define GPIO0_SWPORT_DR_L (GPIO0_BASE + 0x0000)
+#define GPIO0_SWPORT_DDR_L (GPIO0_BASE + 0x0008)
+#define GPIO_REG_LEN (4)
+
+// The base address we're looking for is probably PMU_NS (non-secure PMU)
+#define PMU_BASE (0xFDD90000)
+#define PMU_INT_MASK_CON (PMU_BASE + 0x000C)
+#define PMU_WAKEUP_INT_CON (PMU_BASE + 0x0010)
+#define PMU_WAKEUP_INT_ST (PMU_BASE + 0x0014)
+#define PMU_REG_LEN (4)
+
+#define MAILBOX_BASE (0xFE780000)
+#define MAILBOX_A2B_INTEN (MAILBOX_BASE + 0x0000)
+#define MAILBOX_A2B_STATUS (MAILBOX_BASE + 0x0004)
+#define MAILBOX_A2B_CMD_0 (MAILBOX_BASE + 0x0008)
+#define MAILBOX_A2B_DAT_0 (MAILBOX_BASE + 0x000C)
+
+#define MAILBOX_B2A_INTEN (MAILBOX_BASE + 0x0028)
+#define MAILBOX_B2A_STATUS (MAILBOX_BASE + 0x002C)
+#define MAILBOX_B2A_CMD_0 (MAILBOX_BASE + 0x0030)
+#define MAILBOX_B2A_DAT_0 (MAILBOX_BASE + 0x0034)
+#define MAILBOX_REG_LEN (4)
+
+#define MCU_INTC (0xFE798000)
+#define MCU_INTC_LEN (0xFFFF)
+
+#define SYSTEM_SRAM_BASE (0xFDCC0000)
+#define SYSTEM_SRAM_LEN (0xFFFF)
+
+// You can't reserve GPIO memory regions using request_mem_region (you get an error)
+// but you can just ioremap() them and they work fine. As such, I've turned off
+// requests for this example.
+void* __iomem reserve_iomem(phys_addr_t addr, resource_size_t size) {
+	void* __iomem base;
+	
+	/*if (!request_mem_region(addr, size, "mcu_wakeup")) {
+		pr_err("couldn't request region!\n");
+		return NULL;
+		}*/
+	base = ioremap(addr, size);
+	if (!base) {
+		pr_err("couldn't remap region!\n");
+		return NULL;
+	}
+
+	return base;
+}
+
+void release_iomem(phys_addr_t addr, resource_size_t size) {
+	iounmap((void* __iomem)addr);
+	//release_mem_region(addr, size);
+}
+
+// The issue with the mailbox registers not working was that the clock
+// gate was enabled. This is surely taken care of by the
+// rockchip-mailbox driver.
+static int __init mcu_wakeup_init(void) {
+	void* __iomem system_sram;
+	void* __iomem grf_soc_con3;
+	void* __iomem grf_soc_con4;
+	void* __iomem cru_gate_con32;
+	void* __iomem cru_softrst_con26;
+	void* __iomem mailbox_cmd;
+	void* __iomem mailbox_dat;
+
+	//void* __iomem pmu_wakeup_int_con;
+
+	pr_info("mcu_wakeup: ENTER mcu_wakeup_init()");
+
+	// Disable clock gate on mailbox and INTMUX
+	cru_gate_con32 = reserve_iomem((phys_addr_t)CRU_GATE_CON32, CRU_GATE_CON32_LEN);
+	// write 0 to bits 14 and 15 and enable write to those bits
+	iowrite32((1 << (15+16)) | (1 << (14+16)), cru_gate_con32);
+	release_iomem((phys_addr_t)CRU_GATE_CON32, CRU_GATE_CON32_LEN);
+	
+	// put MCU, mailbox, and INTMUX in reset
+	cru_softrst_con26 = reserve_iomem((phys_addr_t)CRU_SOFTRST_CON26, CRU_SOFTRST_CON26_LEN);
+	pr_info("mcu_wakeup: before reset, cru_softrst_con26 = %x", ioread32(cru_softrst_con26));
+	iowrite32(0xffff0000 | (1 << 12) | (1 << 11) | (1 << 10), cru_softrst_con26);
+	
+	pr_info("mcu_wakeup: MCU, mailbox, INTMUX reset");
+	
+	/*pmu_wakeup_int_con = reserve_iomem((phys_addr_t)PMU_WAKEUP_INT_CON, PMU_REG_LEN);
+	//pr_info("mcu_wakeup: PMU_WAKEUP_INT_CON = %x", ioread32(pmu_wakeup_int_con));
+	// defaults to 0, just enable mcusft
+	iowrite32(1 << 15, pmu_wakeup_int_con);
+	release_iomem((phys_addr_t)PMU_WAKEUP_INT_CON, PMU_REG_LEN);*/
+	
+	// copy RISC-V program into SYSTEM_SRAM
+	system_sram = reserve_iomem((phys_addr_t)SYSTEM_SRAM_BASE, SYSTEM_SRAM_LEN);
+	// zero out SRAM
+	for (int i = 0; i < 0xffff; i += 4) {
+	  iowrite32(0, system_sram + i);
+	}
+	memcpy_toio(system_sram, mcu_wakeup_rv_bin, mcu_wakeup_rv_bin_len);
+	release_iomem((phys_addr_t)SYSTEM_SRAM_BASE, SYSTEM_SRAM_LEN);
+
+	// write the high bits of the boot address to the boot address register
+	grf_soc_con4 = reserve_iomem((phys_addr_t)GRF_SOC_CON4, GRF_SOC_CON4_LEN);
+	iowrite32((0xffff0000) | (SYSTEM_SRAM_BASE >> 16), grf_soc_con4);
+	release_iomem((phys_addr_t)GRF_SOC_CON4, GRF_SOC_CON4_LEN);
+
+	udelay(10); // Rockchip BSP u-boot does this after writing the address
+	
+	// now enable MCU, INTMUX, and mailbox
+	iowrite32(0xffff0000, cru_softrst_con26);
+	release_iomem((phys_addr_t)CRU_SOFTRST_CON26, CRU_SOFTRST_CON26_LEN);
+
+	mdelay(200); // let MCU run
+	
+	// assert interrupt request
+	/*grf_soc_con3 = reserve_iomem((phys_addr_t)GRF_SOC_CON3, GRF_SOC_CON3_LEN);
+	iowrite32((1 << (12+16)) | (1 << 12), grf_soc_con3);
+	release_iomem((phys_addr_t)GRF_SOC_CON3, GRF_SOC_CON3_LEN);*/
+	
+
+	// read mailbox register
+	/*mailbox_cmd = reserve_iomem((phys_addr_t)MAILBOX_B2A_CMD_0, MAILBOX_REG_LEN);
+	mailbox_dat = reserve_iomem((phys_addr_t)MAILBOX_B2A_DAT_0, MAILBOX_REG_LEN);
+	pr_info("mailbox: MAILBOX_B2A_CMD_0 = 0x%x, MAILBOX_B2A_DAT_0 = 0x%x\n",
+		ioread32(mailbox_cmd), ioread32(mailbox_dat));
+	release_iomem((phys_addr_t)MAILBOX_B2A_CMD_0, MAILBOX_REG_LEN);
+	release_iomem((phys_addr_t)MAILBOX_B2A_DAT_0, MAILBOX_REG_LEN);*/
+
+	return 0;
+}
+
+static void __exit mcu_wakeup_exit(void) {
+}
+
+module_init(mcu_wakeup_init);
+module_exit(mcu_wakeup_exit);
+
+MODULE_LICENSE("GPL");
+
---
+++ examples/mcu_wakeup/riscv/mcu_wakeup_rv.c
@@ -0,0 +1,120 @@
+#include <stdint.h>
+
+// This example currently doesn't actually do anything, because I need to find the timer base address!
+#define MAILBOX_BASE (0xFE780000)
+#define MAILBOX_B2A_CMD_0 (MAILBOX_BASE + 0x0030)
+#define MAILBOX_B2A_DAT_0 (MAILBOX_BASE + 0x0034)
+
+
+#define TIMER_BASE (0xFE7F0000)
+#define MCU_TIMER_CTRL (TIMER_BASE + 0x0000)
+#define MCU_TIMER_DIV (TIMER_BASE + 0x0004)
+// timers are rewritable
+#define MCU_MTIME (TIMER_BASE + 0x0008)
+#define MCU_MTIMEH (TIMER_BASE + 0x000C)
+// the first six MCU registers are just memory locations, not CSRs.
+#define MCU_MTIMECMP (TIMER_BASE + 0x0010)
+#define MCU_MTIMECMPH (TIMER_BASE + 0x0014)
+
+#define MCU_BASE (0xFE790000)
+
+#define GPIO0_BASE (0xFDD60000)
+// DR is low/high, DDR is input/output
+#define GPIO0_SWPORT_DR_L (GPIO0_BASE + 0x0000)
+
+#define GPIO3_BASE (0xFE760000)
+#define GPIO3_SWPORT_DR_H (GPIO3_BASE + 0x0004)
+#define GPIO3_SWPORT_DDR_H (GPIO3_BASE + 0x000C)
+
+#define PMU_BASE (0xFDD90000)
+#define PMU_INT_MASK_CON (PMU_BASE + 0x000C)
+
+volatile uint32_t* mailbox_b2a_cmd_0 = (uint32_t*)MAILBOX_B2A_CMD_0;
+volatile uint32_t* mailbox_b2a_dat_0 = (uint32_t*)MAILBOX_B2A_DAT_0;
+
+volatile uint32_t* timer_ctrl = (uint32_t*)MCU_TIMER_CTRL;
+volatile uint32_t* timer_div = (uint32_t*)MCU_TIMER_DIV;
+volatile uint32_t* mtimecmp = (uint32_t*)MCU_MTIMECMP;
+volatile uint32_t* mtimecmph = (uint32_t*)MCU_MTIMECMPH;
+volatile uint32_t* mtime = (uint32_t*)MCU_MTIME;
+volatile uint32_t* mtimeh = (uint32_t*)MCU_MTIMEH;
+
+volatile uint32_t* gpio0_swport_dr_l = (uint32_t*)GPIO0_SWPORT_DR_L;
+volatile uint32_t* gpio3_swport_dr_h = (uint32_t*)GPIO3_SWPORT_DR_H;
+volatile uint32_t* gpio3_swport_ddr_h = (uint32_t*)GPIO3_SWPORT_DDR_H;
+
+volatile uint32_t* pmu_int_mask_con = (uint32_t*)PMU_INT_MASK_CON;
+
+// Under GCC, interrupt functions must have the interrupt attribute
+static void irq_entry() __attribute__ ((interrupt ("machine")));
+
+void toggle_led() {
+  uint32_t reg_value = *gpio3_swport_dr_h;
+  // external LED is GPIO3_C4, or 5th bit of DR_H
+  if (reg_value & (1 << 4)) {
+    *gpio3_swport_dr_h = (1 << (16+4)) | (0 << 4);
+  } else {
+    *gpio3_swport_dr_h = (1 << (16+4)) | (1 << 4);
+  }
+}
+
+// It seems that writing to a GPIO pin, like to enable an LED, competes with Linux
+// because the states conflict. 
+
+// alignment for mtvec.BASE
+#pragma GCC push_options
+// align on a 64-byte boundary because mtvec.BASE is only upper 26
+// bits (32-26 = 6, 2**6 = 64)
+#pragma GCC optimize ("align-functions=64")
+void irq_entry() {
+  uint32_t cause;
+
+  // in direct mode, you have to read mcause
+  __asm__ volatile ("csrr     %0,mcause"
+		    : "=r" (cause) // output into cause
+		    : // no input
+		    : // no clobber
+		    );
+
+  toggle_led();
+  *mtime = 0;
+  *mtimeh = 0;
+  //*mtime += 0x10000;
+  /*uint32_t mtie_enable = 0x80; // set bit 7 (0-indexed)
+  __asm__ volatile ("csrrs    zero, mie, %0"
+		    : // no output
+		    : "r" (mtie_enable) // write mtie_enable to %0
+		    : // no clobber
+		    );
+  
+  uint32_t mie_enable = 0b1000;
+  __asm__ volatile ("csrrs    zero, mstatus, %0"
+		    : // no output
+		    : "r" (mie_enable)
+		    :
+		    );*/
+  /*
+  // wakeup PMU
+  *pmu_int_mask_con = (1 << (15+16)) | (1 << 15);
+  */
+
+  // GCC puts in an mret instruction for us
+}
+#pragma GCC pop_options
+
+int main() {
+
+
+  // set external LED pin to output
+  *gpio3_swport_ddr_h = (1 << (16+4)) | (1 << 4);
+  // I can't get the timer to work, but this does.
+  while (1) {
+    toggle_led();
+    for (uint32_t i = 0; i < 0xffff; i++);
+    toggle_led();
+    for (uint32_t i = 0; i < 0xffff; i++);
+  }
+  *gpio3_swport_dr_h = (1 << (16+4)) | (1 << 4);
+
+  return 0;
+}
